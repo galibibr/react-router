4c5d2094f9ee617f9bfb05384368af67790d7187
GalibIbr <galibiavi4@gmail.com>
2023-12-29 11:52:33 +0300

    filter with useSearchParams

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const BlogPage = () => {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get("post") || "";
+  const latest = searchParams.has('latest')
+
+  const startsFrom = latest ? 80 : 1;
+  console.log(startsFrom);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const query = form.search.value;
+    const isLatest = form.latest.checked;
+
+    const params = {}
+
+    if (query.length) params.post = query
+    if (isLatest) params.latest = true
+
+    setSearchParams(params);
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -13,18 +35,30 @@ const BlogPage = () => {
   return (
     <>
       <h1 className="text-center text-[20px] font-bold">Our posts</h1>
-      <Link to={'/blog/new'}>Add new post</Link>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input type="search" name="search" className="border-b"/>
+        <label className="px-3">
+          <input type="checkbox" name="latest" />
+          New only
+        </label>
+        <input type="submit" value="Search" />
+      </form>
+      <Link to={"/blog/new"} className="hover:underline">
+        Add new post
+      </Link>
       <div className="flex flex-col">
-        {posts.map((post) => {
-          return (
-            <Link
-              key={post.id}
-              to={`/blog/${post.id}`}
-              className="hover:underline">
-              - {post.title}
-            </Link>
-          );
-        })}
+        {posts
+          .filter((e) => e.title.includes(postQuery) && e.id >= startsFrom)
+          .map((post) => {
+            return (
+              <Link
+                key={post.id}
+                to={`/blog/${post.id}`}
+                className="hover:underline">
+                - {post.title}
+              </Link>
+            );
+          })}
       </div>
     </>
   );
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,14 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
   const { signout } = useAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div>
       <h1>CreatePost</h1>
-      <button className="bg-gray-100 rounded-lg px-3 border font-bold" onClick={() => {
-        signout(() => navigate('/', {replace: true}))
-      }}>Log Out</button>
+      <button
+        className="bg-gray-100 rounded-lg px-3 border font-bold"
+        onClick={() => {
+          signout(() => navigate("/", { replace: true }));
+        }}>
+        Log Out
+      </button>
     </div>
   );
 };
